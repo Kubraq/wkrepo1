403836e66ae81ab8d040848c45c3be9e9abffeff
Kubraq <kubraq@poczta.onet.pl>
2015-05-20 16:20:46 +0200

    Cos tam poprawka czegos

---
+++ kalkulator.py
@@ -0,0 +1,12 @@
+def dodawanie(a, b):
+	return a+b
+def get_info():
+	print("To jest prosty kalkulator")
+get_info()
+print('Daj 2 zlote')
+a=int(input())
+
+print('Podaj szebanasta liczbe')
+b=int(input())
+print(dodawanie(a, b))
+
